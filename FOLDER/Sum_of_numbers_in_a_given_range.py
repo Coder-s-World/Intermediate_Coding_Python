731f69ce3621d078521d8360896d4a6e9aad6333
Mayuri Tukadiya <mayuri.tukadiya@gmail.com>
2024-12-31 17:24:17 +0530

    Delete FOLDER directory

--- FOLDER/Sum_of_numbers_in_a_given_range.py
+++
@@ -1,9 +0,0 @@
-# code  Sum of numbers in a given range 
-
-n= int(input("enter min number : "))
-m = int(input("enter max number : "))
-res = 0
-
-for i in range(n,m+1):
-    res = res + i
-print(res)
