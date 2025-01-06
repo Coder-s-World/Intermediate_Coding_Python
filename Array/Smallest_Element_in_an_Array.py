56cdcd75eb6ae3e4338c6a6cf3bb7168ceadefc9
Mayuri Tukadiya <mayuri.tukadiya@gmail.com>
2025-01-06 13:44:08 +0530

    Add files via upload

--- Array/Smallest_Element_in_an_Array.py
+++ Array/Smallest_Element_in_an_Array.py
@@ -1,12 +1,16 @@
 # find the smallest element from the given array.
 
 array = [ 15, 26, -54678, -5, -7, -9294542, 16, 15 ]
-
 smallest = array[0]
 
 for num in array:
     if num < smallest :
         smallest = num
 print(f"{smallest} is the smallest element")
-           
-        
+
+# method 2
+print(f"the smallest element from the array is {min(array)}")
+
+# method 3
+small = array.sort()
+print(f"the smallest element is {array[0]}")      
