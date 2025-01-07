a1bc3522ca0974f69389772c3696261934c95a0b
Mayuri Tukadiya <mayuri.tukadiya@gmail.com>
2025-01-07 14:02:03 +0530

    Add files via upload

---
+++ Advanced_in_Numbers/binary_to_decimal_conversion_of_number.py
@@ -0,0 +1,30 @@
+# binary number - A binary number is a number expressed in the base-2 numeral system
+# decimal number - A decimal is a number that consists of a whole and a fractional part.
+
+binary_num = int(input("enter binary number : "))
+i = 0
+calc = 0
+while binary_num != 0 :
+    rem = binary_num % 10 
+    calc = pow(2,i) * rem + calc
+    i += 1
+    binary_num //= 10
+    
+print(f"{calc} is the conversion of binary to decimal number")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
