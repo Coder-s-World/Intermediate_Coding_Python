13f2d21258601ccdaf571aee5a4e55b4ff161f34
Mayuri Tukadiya <mayuri.tukadiya@gmail.com>
2025-01-07 14:17:33 +0530

    Update binary_to_decimal_conversion_of_number.py

--- Advanced_in_Numbers/binary_to_decimal_conversion_of_number.py
+++ Advanced_in_Numbers/binary_to_decimal_conversion_of_number.py
@@ -4,9 +4,11 @@
 binary_num = int(input("enter binary number : "))
 i = 0
 calc = 0
+base = 2
+
 while binary_num != 0 :
     rem = binary_num % 10 
-    calc = pow(2,i) * rem + calc
+    calc = pow(base,i) * rem + calc
     i += 1
     binary_num //= 10
     
